c8a270004308d84bcc4cc9297342d36fcf7999a4
Zeeshan412 <101563589+Zeeshan412@users.noreply.github.com>
2025-07-08 14:35:57 +0500

    some more update in the code

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('✅ MongoDB connected');
+    console.log('MongoDB successfully connected');
   } catch (err) {
     console.error('MongoDB connection failed:', err.message);
     process.exit(1);
